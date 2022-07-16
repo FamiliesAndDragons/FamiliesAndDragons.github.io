7881ea8f68c31d47bd507348d4af8494f97d9d14
CantFindGeorge <109295935+CantFindGeorge@users.noreply.github.com>
2022-07-16 11:46:31 -0400

    character db
    added google sheet test

---
+++ js/table.js
@@ -0,0 +1,20 @@
+
+
+var data = [
+    [1, "jack",55],
+    [2, "anna", 11]
+];
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    var tbody = document.getElementById("table-body");
+
+    var row = document.createElement("tr");
+    var col1 = document.createElement("td");
+    col1.textContent - "apple"
+    row.appendChild(col1);
+    tbody.appendChild(row);
+
+
+
+});
